4c3bf8c61f5372f4759f2865214871712124fd8a
Omar Rodriguez <orodriguez@peruapps.com.pe>
2025-01-13 12:05:52 -0500

    fix error on prerenderer

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,9 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
+    renderMode: RenderMode.Client // https://blog.ninja-squad.com/2024/11/19/what-is-new-angular-19.0/
   }
+
+
+
 ];
